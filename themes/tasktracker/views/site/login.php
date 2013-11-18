1a6c9f13188d55633b3d58e13f79d470a8e854c8
amurwebmaster <amurwebmaster@gmail.com>
2013-11-18 16:47:44 +0700

    16:46 Исправлено: 1. в форме Входа не заполнить поля Реальный результат: появляются сообщения об ошибках на русском языке 2. заполнить валидными данными поле напр Имя пользователя 3. заполнить невалидными данными поле Пароль 4. нажать на кнопку Login Реальный результат: сообщения на англ языке Ожидаемый результат: сообщения должны выводиться на языке, в соответствии с языковой версией продукта.

--- themes/tasktracker/views/site/login.php
+++ themes/tasktracker/views/site/login.php
@@ -3,15 +3,15 @@
 /* @var $model LoginForm */
 /* @var $form CActiveForm  */
 
-$this->pageTitle=Yii::app()->name . ' - Login';
+$this->pageTitle=Yii::app()->name . ' - Вхлд на сайт';
 $this->breadcrumbs=array(
-	'Login',
+	'Вход',
 );
 ?>
 
-<h1>Login</h1>
+<h1>Вход на сайт!</h1>
 
-<p>Please fill out the following form with your login credentials:</p>
+<p>Пожалуйста заполните поля "логин" и "пароль", для входа на сайт:</p>
 
 <div class="form">
 
@@ -24,13 +24,9 @@ $this->breadcrumbs=array(
 	),
 )); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
-
 	<?php echo $form->textFieldRow($model,'username'); ?>
 
-	<?php echo $form->passwordFieldRow($model,'password',array(
-        'hint'=>'Hint: You may login with <kbd>demo</kbd>/<kbd>demo</kbd> or <kbd>admin</kbd>/<kbd>admin</kbd>',
-    )); ?>
+	<?php echo $form->passwordFieldRow($model,'password'); ?>
 
 	<?php echo $form->checkBoxRow($model,'rememberMe'); ?>
 
@@ -38,7 +34,7 @@ $this->breadcrumbs=array(
 		<?php $this->widget('bootstrap.widgets.TbButton', array(
             'buttonType'=>'submit',
             'type'=>'primary',
-            'label'=>'Login',
+            'label'=>'Вход',
         )); ?>
 	</div>
 
